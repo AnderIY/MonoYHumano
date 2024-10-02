d0f7aa09f31d2ce52544ac5621781536e29be1f6
AnderIY <andersancheziy@gmail.com>
2024-10-02 17:04:31 +0200

    clases

---
+++ src/interfaz/Camarero.java
@@ -0,0 +1,12 @@
+package interfaz;
+
+public class Camarero {
+	private String nombre;
+	private boolean activo;
+	
+	public Camarero(String nombre) {
+		this.nombre = nombre;
+		this.activo = activo;
+		
+	}
+}
